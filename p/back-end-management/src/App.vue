98260b0c8ecad67399d0edd329fcd5e4db7f9489
ysy <youngshuyu1997@gmail.com>
2020-07-01 21:40:50 +0800

    init

---
+++ p/back-end-management/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <v-main></v-main>
+  </div>
+</template>
+
+<script>
+import main from './components/main'
+export default {
+  name: 'app',
+  data () {
+    return {}
+  },
+  components: {
+    'v-main': main
+  }
+}
+</script>
+<style lang="less">
+
+</style>
---
+++ p/back-end-management/src/components/main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>oa</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
